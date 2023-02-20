131cc00b7c39d0cb635f662ec47f201b22f0024b
jhs512 <jangka512@gmail.com>
2023-02-20 17:59:48 +0900

    세팅

---
+++ src/main/java/com/ll/hogamapp/HogamappApplication.java
@@ -0,0 +1,13 @@
+package com.ll.hogamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogamappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogamappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/hogamapp/HogamappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.hogamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogamappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
